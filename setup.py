e3ed87f6afca3c82edcf003cdcf2aece8706a75f
Cameron Aldren <cameron.aldren19@imperial.ac.uk>
2025-04-28 15:33:10 +0100

    Rename to h2-gym

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="vc_gym",
+    name="h2_gym",
     version="0.1",
     packages=find_packages(where="src"),
     package_dir={"":"src"},
     include_package_data=True,
     package_data={
-        'vc_gym': ['data/*.csv'],  # adjust as needed
+        'h2_gym': ['data/*.csv'],  # adjust as needed
     },
     install_requires = [
         "meteor_py  @ git+https://github.com/cja119/meteor_py.git",
         "numpy",
-        "jax"
+        "jax",
+        "mu_F @ git+https://github.com/mawbray/mu.F.git"
     ]
 )
