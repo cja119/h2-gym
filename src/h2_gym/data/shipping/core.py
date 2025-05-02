aaa6392c8c8b36fdb5f94b7794f74b550e7bdf02
Cameron Aldren <cameron.aldren19@imperial.ac.uk>
2025-05-02 11:04:21 +0100

    Added a submodule for natural gas demand (for shipping env)

--- src/h2_gym/data/shipping/core.py
+++ src/h2_gym/data/shipping/core.py
@@ -3,10 +3,7 @@ This class will pull the natural gas data for different regions and 'clean'
 it into a format that cna be used in the shipping model.
 """
 from __future__ import annotations
-from typing import Optional 
-from pathlib import Path
-import pandas as pd
-from pycountry import countries
+
 
 class NGDemand:
 
@@ -24,29 +21,7 @@ class NGDemand:
 
     
     @staticmethod
-    def get_data(data, path, country, unit) -> str:
-        """
-        Returns the data of the natural gas demand class
-        """
-        if data[-4:] != '.csv':
-            data = data + '.csv'
-
-        # Handling the path
-        path = Path(path) if path is not None else Path(__file__).parent / 'csv'
-        dataset = pd.read_csv(path / data)
-
-        # Updating the column names to the country names
-        dataset.set_index("REF_AREA", inplace=True)
-        dataset.index = dataset.index.map(
-            lambda i: countries.get(alpha_2=i.upper()).name
-            )
-
-        # Finding LNG import data, filtering by unit and country
-        dataset = dataset.loc[country][
-            (dataset.loc[country]['FLOW_BREAKDOWN'] == 'IMPLNG')\
-                & (dataset.loc['United Kingdom']['UNIT_MEASURE'] == unit)
-                ]
-        return dataset
+    
     
     def data(self) -> pd.DataFrame:
         """
